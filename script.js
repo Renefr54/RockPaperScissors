a92698a344ab850a1b0a25ee2e10b9334ac85f31
Renefr54 <renefr54@gmail.com>
2023-07-03 07:25:19 -0700

    Create function and while loop to play rock paper scissors for 5 rounds

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-function playground (){
-    getComputerChoice();
-    getPlayerChoice();
+function game() {
+    
+    let compScore = 0;
+    let userScore = 0;
 
-    if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-        return 'You win, ' + playerSelection + ' beats ' + computerSelection;
-    } else if (
-        (playerSelection === 'scissors' && computerSelection === 'rock') ||
-        (playerSelection === 'rock' && computerSelection === 'paper') ||
-        (playerSelection === 'paper' && computerSelection === 'scissors')
-    ) {
-        return 'The computer wins! ' + computerSelection + ' beats ' + playerSelection;
+    while (compScore < 5 || userScore < 5) {
+        const result = playground();
+        console.log(result)
+        
+        if (result.includes('You win')) {
+            userScore++;
+        } else if (result.includes('The computer wins! ')) {
+            compScore++;
+        }
+    }
+
+    if (userScore === 5) {
+        console.log('Congrats, you won')
     } else {
-        return "It's a tie";
+        console.log('The computer wins')
+    }
+    function playground (){
+        getComputerChoice();
+        getPlayerChoice();
+    
+        if (
+            (playerSelection === 'rock' && computerSelection === 'scissors') ||
+            (playerSelection === 'paper' && computerSelection === 'rock') ||
+            (playerSelection === 'scissors' && computerSelection === 'paper')
+        ) {
+            return 'You win, ' + playerSelection + ' beats ' + computerSelection + 'The score is User: ' + (userScore + 1) + 'to Computer Score: ' + (compScore + 1);
+        } else if (
+            (playerSelection === 'scissors' && computerSelection === 'rock') ||
+            (playerSelection === 'rock' && computerSelection === 'paper') ||
+            (playerSelection === 'paper' && computerSelection === 'scissors')
+        ) {
+            return 'The computer wins! ' + computerSelection + ' beats ' + playerSelection + 'The score is User: ' + (userScore + 1) + 'to Computer Score: ' + (compScore + 1);
+        } else {
+            return "It's a tie";
+        }
     }
 }
+
+
 
 function getComputerChoice (randomNumber){
     randomNumber = Math.floor(Math.random() * 3);
@@ -33,8 +57,6 @@ function getComputerChoice (randomNumber){
 
 function getPlayerChoice() {
     playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
-    console.log(playerSelection);
 }
 
-
-console.log(playground())
+console.log(game())
